5197004a7780d2866a6d95aede1fb012e211b61e
NouraAlahmadi12 <91524062+NouraAlahmadi12@users.noreply.github.com>
2021-12-25 19:50:43 +0300

    ..# (#4)
    - Create func showError(errorView:String) -> to show error massege for the user
     - Create func presentChatScreen() -> to present the chat screen after user log in or sign up
    
    Co-authored-by: Nora <Nouraalahmadi1@gmail.com>

--- LoopChat/LoopChat1/Controller/SettignScreenVC.swift
+++ LoopChat/LoopChat1/Controller/SettignScreenVC.swift
@@ -6,31 +6,31 @@
 //
 
 import UIKit
+import Firebase
 class SettignScreenVC: UIViewController {
     
     @IBOutlet weak var darkAndLightMood: UISwitch!
-    
     @IBAction func darkAndLightMoodAction(_ sender: Any) {
         
         if(darkAndLightMood.isOn == true){
             let appView = UIApplication.shared.windows.first
             appView?.overrideUserInterfaceStyle = .light
         }else{
-            
             let appView = UIApplication.shared.windows.first
             appView?.overrideUserInterfaceStyle = .dark
-    }
+        }
     }
     @IBAction func logoutButton(_ sender: Any) {
-        
+        try! Auth.auth().signOut()
+        presentHomeScreen()
+    }
+    
+    func presentHomeScreen (){
         let homeScreem = storyboard?.instantiateViewController(withIdentifier: "HomeScreenID") as! SignupAndLoginVC
         present(homeScreem, animated: true, completion: nil)
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         
     }
 }
