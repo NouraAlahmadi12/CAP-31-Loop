63f7383247126a650824e6aafc533080f0a18139
NouraAlahmadi12 <91524062+NouraAlahmadi12@users.noreply.github.com>
2022-01-07 11:50:14 +0300

    .....# (#14)
    - Create func observeCommunity()
     - Create class CommunityTVC
     - Create enum UserType in user Model
     - Add table View in communityVC to view community
    
    Co-authored-by: Nora <Nouraalahmadi1@gmail.com>

---
+++ LoopChat/LoopChat1/Cell/CommunityTVC.swift
@@ -0,0 +1,14 @@
+//
+//  CommunityTVC.swift
+//  LoopChat1
+//
+//  Created by Noura Alahmadi on 03/06/1443 AH.
+//
+
+import UIKit
+
+class CommunityTVC: UITableViewCell {
+
+    @IBOutlet weak var communityName: UILabel!
+    
+}
--- LoopChat/LoopChat1/Controller/CommunityVC.swift
+++ LoopChat/LoopChat1/Controller/CommunityVC.swift
@@ -7,26 +7,44 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestore
 import FirebaseFirestoreSwift
-class CommunityVC: UIViewController ,UICollectionViewDelegate , UICollectionViewDataSource{
+class CommunityVC: UIViewController , UITableViewDataSource , UITableViewDelegate{
     
-    @IBOutlet weak var roomOfCommunityCV: UICollectionView!
+    let db = Firestore.firestore()
+    var communities = [Community]()
     
-    var communityArr = CreateScreenVC().communities
+    @IBOutlet weak var newCommunityName: UITextField!
+    @IBOutlet weak var CommunityTableView: UITableView!
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return communityArr.count
+    @IBAction func createNewCommunityButton(_ sender: Any) {
+        
+        guard let nameOfCommunity = newCommunityName.text , nameOfCommunity.isEmpty == false else {
+            return
+        }
+        
+        let databaseRefrence = Database.database().reference()
+        let communities = databaseRefrence.child("communities").childByAutoId()
+        
+        let dataArray: [String : Any] = ["Community Name": nameOfCommunity]
+        communities.setValue(dataArray) { error, ref in
+            self.newCommunityName.text = ""
+        }
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = roomOfCommunityCV.dequeueReusableCell(withReuseIdentifier: "CommunityCellID", for: indexPath) as! CommunityCVCell
-        cell.getInfo(community: communityArr[indexPath.row])
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let viewController = (storyboard?.instantiateViewController(withIdentifier: "ChatScreenID")) as! ChatScreentVC
-        self.navigationController?.pushViewController(viewController, animated: true)
+    func observeCommunity(){
+        
+        let databaseRef = Database.database().reference()
+        databaseRef.child("communities").observe(.childAdded){(snapshot) in
+            print(snapshot)
+            if let dataArray = snapshot.value as? [String: Any]{
+                if let communityName = dataArray["communitytName"] as? String{
+                    let community = Community.init(communityName: self.newCommunityName.text! , communityMember: [])
+                    self.communities.append(community)
+                    self.CommunityTableView.reloadData()
+                }
+            }
+        }
     }
     
     func presentLoginScreen (){
@@ -36,18 +54,28 @@ class CommunityVC: UIViewController ,UICollectionViewDelegate , UICollectionView
     }
     
     
+    // MARK: tableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return communities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = CommunityTableView.dequeueReusableCell(withIdentifier: "CommunityCellID", for: indexPath) as! CommunityTVC
+        cell.communityName.text = communities[indexPath.row].communityName
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CommunityTableView.delegate = self
+        CommunityTableView.dataSource = self
+        observeCommunity()
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         let currentUser = Auth.auth().currentUser
         if (currentUser?.uid == nil ) {
             presentLoginScreen()
         }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        roomOfCommunityCV.delegate = self
-        roomOfCommunityCV.dataSource = self
-        roomOfCommunityCV.reloadData()
-        
     }
 }
